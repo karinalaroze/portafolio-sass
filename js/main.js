4de08da81a38f31d351c3c4cad9139f08968507f
KarinaLaroze <karinalaroze@gmail.com>
2017-07-24 12:55:05 -0400

    avanzando en el trabajo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
 	$('.slider').slider();
+	
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 300) {
+	    $(".navbar").css("background","whitesmoke");
+	    $(".navbar  ul li a").css("color", "grey");
+	    $(".brand-logo").css("color", "grey");
+	  }
+
+	  else{
+		  $(".navbar").css("background" , "transparent"); 
+		  $(".navbar  ul li a").css("color", "white");
+	    $(".brand-logo").css("color", "white"); 	
+	  }
+  })
 });
